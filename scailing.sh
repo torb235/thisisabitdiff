5034b8a77273abaa0ce1a866e1a73207dd9c9ee5
torb235 <74792520+torb235@users.noreply.github.com>
2022-06-24 19:27:10 +0300

    Update scailing.sh

--- scailing.sh
+++ scailing.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd tailscale_1.26.1_amd64 && ./tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1055 & ./tailscale up --authkey=${TAILSCALE_AUTHKEY} &> /dev/null &
+cd tailscale_1.26.1_amd64
+./tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1055 & ./tailscale up --authkey=${TAILSCALE_AUTHKEY} &>/dev/null &
 HTTP_PROXY=http://localhost:8055/
